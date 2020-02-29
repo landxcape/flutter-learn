d9485403960fa21a25c02d3bd3de3670ff75c571
Ashish <landxcape09@gmail.com>
2020-02-29 17:23:26 +0545

    auto-shuffling problem

--- nursery_science/lib/homepage.dart
+++ nursery_science/lib/homepage.dart
@@ -30,109 +30,112 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Nursery Science'),
-        elevation: defaultTargetPlatform == TargetPlatform.android ? 5.0 : 0.0,
-      ),
-      drawer: Drawer(
-        child: ListView(
-          children: <Widget>[
-            UserAccountsDrawerHeader(
-              accountName: Text('Student Name'),
-              accountEmail: Text('studentemail@schoolmail.com'),
-              currentAccountPicture: CircleAvatar(
-                backgroundColor:
-                    Theme.of(context).platform == TargetPlatform.iOS
-                        ? Colors.accents
-                        : Colors.amber,
-                child: Text('S'),
-              ),
-              otherAccountsPictures: <Widget>[
-                CircleAvatar(
-                  backgroundColor:
-                      Theme.of(context).platform == TargetPlatform.iOS
-                          ? Colors.accents
-                          : Colors.amber,
-                  child: Text('T'),
-                ),
-                CircleAvatar(
-                  backgroundColor:
-                      Theme.of(context).platform == TargetPlatform.iOS
-                          ? Colors.accents
-                          : Colors.amber,
-                  child: Text('U'),
-                ),
-              ],
-            ),
-            ListTile(
-              title: Text('Who Are You?'),
-              leading: Icon(Icons.people),
-              onTap: () {
-                Navigator.of(context).popAndPushNamed('/whoareyou');
-                // OR,
-                // Navigator.of(context).push(MaterialPageRoute(
-                //     builder: (BuildContext context) => WhoAreYou()));
-              },
-            ),
-            ListTile(
-              title: Text('Our Sense Organs!'),
-              leading: Icon(Icons.face),
-              onTap: () {
-                Navigator.of(context).popAndPushNamed('/oursenseorgans');
-              },
-            ),
-            ListTile(
-              title: Text('What can you Smell?'),
-              leading: Icon(Icons.face),
-              onTap: () {
-                Navigator.of(context).popAndPushNamed('/whatcanyousmell');
-              },
-            ),
-            ListTile(
-              title: Text('What can you See?'),
-              leading: Icon(Icons.home),
-              onTap: () {
-                Navigator.of(context).popAndPushNamed('/whatcanyousee');
-              },
-            ),
-            ListTile(
-              title: Text('What can you Hear?'),
-              leading: Icon(Icons.phone),
-              onTap: () {
-                Navigator.of(context).popAndPushNamed('/whatcanyouhear');
-              },
-            ),
-            ListTile(
-              title: Text('What can you Taste?'),
-              leading: Icon(Icons.fastfood),
-              onTap: () {
-                Navigator.of(context).popAndPushNamed('/whatcanyoutaste');
-              },
-            ),
-            ListTile(
-              title: Text('Match Same Pictures'),
-              leading: Icon(Icons.picture_in_picture),
-              onTap: () {
-                Navigator.of(context).popAndPushNamed('/matchpictures');
-              },
-            ),
-            Divider(),
-            ListTile(
-              title: Text('Logout'),
-              leading: Icon(Icons.exit_to_app),
-              onTap: () {
-                Navigator.of(context).pop();
-                Navigator.of(context).pushReplacementNamed('/loginpage');
-              },
-            ),
-          ],
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text('Nursery Science'),
+          elevation:
+              defaultTargetPlatform == TargetPlatform.android ? 5.0 : 0.0,
         ),
-      ),
-      body: Center(
-        child: Container(
-          child: Text(
-            'HomePage',
+        drawer: Drawer(
+          child: ListView(
+            children: <Widget>[
+              UserAccountsDrawerHeader(
+                accountName: Text('Student Name'),
+                accountEmail: Text('studentemail@schoolmail.com'),
+                currentAccountPicture: CircleAvatar(
+                  backgroundColor:
+                      Theme.of(context).platform == TargetPlatform.iOS
+                          ? Colors.accents
+                          : Colors.amber,
+                  child: Text('S'),
+                ),
+                otherAccountsPictures: <Widget>[
+                  CircleAvatar(
+                    backgroundColor:
+                        Theme.of(context).platform == TargetPlatform.iOS
+                            ? Colors.accents
+                            : Colors.amber,
+                    child: Text('T'),
+                  ),
+                  CircleAvatar(
+                    backgroundColor:
+                        Theme.of(context).platform == TargetPlatform.iOS
+                            ? Colors.accents
+                            : Colors.amber,
+                    child: Text('U'),
+                  ),
+                ],
+              ),
+              ListTile(
+                title: Text('Who Are You?'),
+                leading: Icon(Icons.people),
+                onTap: () {
+                  Navigator.of(context).popAndPushNamed('/whoareyou');
+                  // OR,
+                  // Navigator.of(context).push(MaterialPageRoute(
+                  //     builder: (BuildContext context) => WhoAreYou()));
+                },
+              ),
+              ListTile(
+                title: Text('Our Sense Organs!'),
+                leading: Icon(Icons.face),
+                onTap: () {
+                  Navigator.of(context).popAndPushNamed('/oursenseorgans');
+                },
+              ),
+              ListTile(
+                title: Text('What can you Smell?'),
+                leading: Icon(Icons.face),
+                onTap: () {
+                  Navigator.of(context).popAndPushNamed('/whatcanyousmell');
+                },
+              ),
+              ListTile(
+                title: Text('What can you See?'),
+                leading: Icon(Icons.home),
+                onTap: () {
+                  Navigator.of(context).popAndPushNamed('/whatcanyousee');
+                },
+              ),
+              ListTile(
+                title: Text('What can you Hear?'),
+                leading: Icon(Icons.phone),
+                onTap: () {
+                  Navigator.of(context).popAndPushNamed('/whatcanyouhear');
+                },
+              ),
+              ListTile(
+                title: Text('What can you Taste?'),
+                leading: Icon(Icons.fastfood),
+                onTap: () {
+                  Navigator.of(context).popAndPushNamed('/whatcanyoutaste');
+                },
+              ),
+              ListTile(
+                title: Text('Match Same Pictures'),
+                leading: Icon(Icons.picture_in_picture),
+                onTap: () {
+                  Navigator.of(context).popAndPushNamed('/matchpictures');
+                },
+              ),
+              Divider(),
+              ListTile(
+                title: Text('Logout'),
+                leading: Icon(Icons.exit_to_app),
+                onTap: () {
+                  Navigator.of(context).pop();
+                  Navigator.of(context).pushReplacementNamed('/loginpage');
+                },
+              ),
+            ],
+          ),
+        ),
+        body: Center(
+          child: Container(
+            child: Text(
+              'HomePage',
+            ),
           ),
         ),
       ),
--- nursery_science/lib/match_same_pictures.dart
+++ nursery_science/lib/match_same_pictures.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
+import 'package:flutter/services.dart';
 
 import 'dart:math';
 import 'package:audioplayers/audio_cache.dart';
@@ -22,8 +23,11 @@ class _MatchPicturesState extends State<MatchPictures> {
 
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+    ]);
     return Scaffold(
-      appBar: AppBar(title: Text('Match Same Pictures')),
+      // appBar: AppBar(title: Text('Match Same Pictures')),
       body: SafeArea(child: MatchTable(senseOrgans: senseOrgans)),
     );
   }
@@ -40,67 +44,196 @@ class MatchTable extends StatefulWidget {
 
 class _MatchTableState extends State<MatchTable> {
   Map senseOrgans;
+  Map<String, bool> score = {};
+  int _maxLives = 3;
   AudioCache _audioController = AudioCache();
 
   _MatchTableState({this.senseOrgans});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: EdgeInsets.symmetric(horizontal: 10.0),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: <Widget>[
-          Expanded(
-            flex: 1,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: senseOrgans.keys
-                  .map((organ) => _buildDraggableImages(organ))
-                  .toList(),
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.start,
+      children: <Widget>[
+        Expanded(
+          flex: 1,
+          child: Container(
+            child: RichText(
+              text: TextSpan(
+                style: TextStyle(
+                  color: Colors.pink,
+                  fontSize: 25.0,
+                  fontWeight: FontWeight.bold,
+                  fontStyle: FontStyle.italic,
+                ),
+                children: <TextSpan>[
+                  TextSpan(
+                    text: 'Match ',
+                  ),
+                  TextSpan(
+                    text: 'Same',
+                    style: TextStyle(
+                      color: Colors.pink,
+                      fontSize: 50.0,
+                      fontWeight: FontWeight.bold,
+                      fontStyle: FontStyle.italic,
+                    ),
+                  ),
+                  TextSpan(
+                    text: ' Pictures',
+                  ),
+                ],
+              ),
             ),
           ),
-          Expanded(
-            flex: 1,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              crossAxisAlignment: CrossAxisAlignment.end,
-              children: senseOrgans.keys
-                  .map((organ) => _buildDragTarget(organ))
-                  .toList()
-                    ..shuffle(Random()),
+        ),
+        Expanded(
+          flex: 1,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Expanded(
+                child: Container(
+                  alignment: Alignment.centerRight,
+                  decoration: BoxDecoration(
+                    gradient: LinearGradient(
+                      tileMode: TileMode.mirror,
+                      colors: [
+                        Colors.white,
+                        Colors.pink,
+                      ],
+                    ),
+                    border: Border(
+                      right: BorderSide(
+                        color: Colors.deepOrange,
+                      ),
+                    ),
+                  ),
+                  child: RichText(
+                    text: TextSpan(
+                      text: ' :Score ',
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 25.0,
+                        fontWeight: FontWeight.bold,
+                        fontStyle: FontStyle.italic,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              Expanded(
+                child: Container(
+                  alignment: Alignment.centerLeft,
+                  decoration: BoxDecoration(
+                    gradient: LinearGradient(
+                      tileMode: TileMode.mirror,
+                      colors: [
+                        Colors.pink,
+                        Colors.white,
+                      ],
+                    ),
+                    border: Border(
+                      left: BorderSide(
+                        color: Colors.deepOrange,
+                      ),
+                    ),
+                  ),
+                  child: Row(
+                    children: <Widget>[
+                      RichText(
+                        text: TextSpan(
+                          text: ' Lifes: ',
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 25.0,
+                            fontWeight: FontWeight.bold,
+                            fontStyle: FontStyle.italic,
+                          ),
+                        ),
+                      ),
+                      LimitedBox(
+                        maxWidth: 100.0,
+                        maxHeight: 100.0,
+                        child: ListView.builder(
+                          scrollDirection: Axis.horizontal,
+                          itemCount: _maxLives,
+                          itemBuilder: (BuildContext context, int index) =>
+                              Icon(Icons.favorite),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+        Expanded(
+          flex: 10,
+          child: SingleChildScrollView(
+            child: LimitedBox(
+              maxHeight: MediaQuery.of(context).size.height * (9.8 / 12.5),
+              child: Container(
+                margin: EdgeInsets.symmetric(horizontal: 10.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Expanded(
+                      flex: 1,
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: senseOrgans.keys
+                            .map((organ) => _buildDraggableImages(organ))
+                            .toList(),
+                      ),
+                    ),
+                    Expanded(
+                      flex: 1,
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        crossAxisAlignment: CrossAxisAlignment.end,
+                        children: senseOrgans.keys
+                            .map((organ) => _buildDragTarget(organ))
+                            .toList()
+                              ..shuffle(Random()),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
             ),
           ),
-        ],
-      ),
+        ),
+      ],
     );
   }
 
   Widget _buildDraggableImages(organ) {
     double contentWidth = MediaQuery.of(context).size.width * 0.3;
 
-    return Container(
-      constraints: BoxConstraints(
-        maxHeight: contentWidth,
-        maxWidth: contentWidth,
-      ),
-      child: Draggable(
-        onDragCompleted: () {},
-        data: organ.toString(),
+    return Draggable(
+      onDragCompleted: () {},
+      data: organ.toString(),
+      child: Container(
+        constraints: BoxConstraints(
+          maxHeight: contentWidth,
+          maxWidth: contentWidth,
+        ),
         child: Image.asset(
           senseOrgans[organ],
           fit: BoxFit.scaleDown,
         ),
-        feedback: Container(
-          constraints: BoxConstraints(
-            maxHeight: contentWidth,
-            maxWidth: contentWidth,
-          ),
-          child: Image.asset(
-            senseOrgans[organ],
-            fit: BoxFit.scaleDown,
-          ),
+      ),
+      feedback: Container(
+        constraints: BoxConstraints(
+          maxHeight: contentWidth,
+          maxWidth: contentWidth,
+        ),
+        child: Image.asset(
+          senseOrgans[organ],
+          fit: BoxFit.scaleDown,
         ),
       ),
     );
@@ -179,6 +312,9 @@ class _MatchTableState extends State<MatchTable> {
           } else {
             _audioController.play('sounds/wrong_buzzer.mp3');
             correct = 'false';
+            setState(() {
+              _maxLives--;
+            });
           }
           // tried = true; // no second try
           tried = false; // multiple tries
